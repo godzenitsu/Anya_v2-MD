10583056d265e8574ad6c62ffe70724774781c68
godzenitsu <164893015+godzenitsu@users.noreply.github.com>
2024-03-29 15:18:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const { readFileSync } = require('fs')
 require("dotenv").config();
 
 let badWords = [
-  "vagina",
+  "puri",
   "dick",
   "mdrchod",
   "mdrchod",
@@ -14,7 +14,7 @@ let badWords = [
   "hoes",
   "lode",
   "cum",
-  "idiot",
+  "ninte appan",
   "bastard",
   "cunt",
   "butt",
@@ -24,9 +24,9 @@ let badWords = [
   "scum",
   "scumbag",
   "niggr",
-  "nigga",
+  "punde",
   "chod",
-  "bhenchod",
+  "umban",
   "bc",
   "bhodike",
   "bsdk","randi",
@@ -63,16 +63,16 @@ global.message = {
 },
 
 module.exports = {
-  botname: process.env.BotName || "Queen Anya", 
-  author: process.env.Author || "@PikaBotz",
-  packname: process.env.PackName || "Queen Anya v2 MD",
-  socialLink: process.env.Web || "https://github.com/PikaBotz",
-  footer: process.env.Footer || "© Queen Anya Bot",
-  prefa: process.env.Prefix || ['-'],
+  botname: process.env.BotName || "Queen nezuko ", 
+  author: process.env.Author || "@whoois.zenitsu",
+  packname: process.env.PackName || "Queen nezuko md",
+  socialLink: process.env.Web || "https://github.com/godzenitsu",
+  footer: process.env.Footer || "© Queen nezuko",
+  prefa: process.env.Prefix || [','],
   themeemoji: process.env.ThemeEmoji || "🎐",
-  ownername: process.env.Owner_Name || "Pika~Kun",
-  ownernumber: process.env.Owner_Number || "916900904828",
-  instagramId: process.env.Insta || "8.08_only_mine",
+  ownername: process.env.Owner_Name || "𝗚𝗢𝗗 𝗭𝗘𝗡𝗜𝗧𝗦𝗨",
+  ownernumber: process.env.Owner_Number || "919744108970",
+  instagramId: process.env.Insta || "whoois.zenitsu",
   warns: process.env.Warns_Limits || 3,
   mongoUrl: process.env.MongoDB || "YOUR_MONGODB_URL",
   welcome: process.env.Welcome_Msg || '*@$user* joined this group today as $membersth member.\n\n_$prefix welcome off to disable this message._',
@@ -80,11 +80,11 @@ module.exports = {
   promote: process.env.Promote_Msg || '*@$user* has been promoted as an admin in this group.\n\n_$prefix promotem off to disable this message._',
   demote: process.env.Demote_Msg || '*@$user* has been demoted to a member in this group.\n\n_$prefix demotem off to disable this message._',
   sessionId: process.env.SESSION_ID || "YOUR_SESSION_ID", 
-  image_1: readFileSync('./lib/Assets/image_1.jpg'), // Thumbnail for allmenu command
-  image_2: readFileSync('./lib/Assets/image_2.jpg'), // null image
-  image_3: readFileSync("./lib/Assets/image_3.jpg"), // Thumbnail for Dashboard
-  aliveMedia: readFileSync("./lib/Assets/aliveMedia.mp4"),
-  menuMedia: readFileSync('./lib/Assets/menuMedia.mp4'),
+  image_1: readFileSync('./lib/IMG-20240329-WA0105.jpg'), // Thumbnail for allmenu command
+  image_2: readFileSync('./lib/IMG-20240329-WA0105.jpg'), // null image
+  image_3: readFileSync('./lib/IMG-20240329-WA0105.jpg')), // Thumbnail for Dashboard
+  aliveMedia: https://i.imgur.com/lGkriSF.jpeg,
+  menuMedia: readFileSync('./lib/IMG-20240329-WA0105.jpg'),
   badWords: badWords,
   message: {
     success: message.success,
@@ -105,19 +105,19 @@ module.exports = {
 
 
 // Ignore them 👇🏻
-global.botname = process.env.BotName || "Queen Anya" 
-global.author = process.env.Author || "@PikaBotz" 
-global.packname = process.env.PackName || "Queen Anya v2 MD" 
-global.myweb = process.env.Web || "https://github.com/PikaBotz" 
-global.footer = process.env.Footer || "© Queen Anya Bot" 
-global.prefa = process.env.Prefix || ['-'] 
+global.botname = process.env.BotName || "Queen nezuko" 
+global.author = process.env.Author || "@zenitsu" 
+global.packname = process.env.PackName || "Queen nezuko v2 MD" 
+global.myweb = process.env.Web || "https://github.com/godzenitsu" 
+global.footer = process.env.Footer || "© Queen nezuko Bot" 
+global.prefa = process.env.Prefix || [','] 
 global.themeemoji = process.env.ThemeEmoji || "🎐" 
-global.ownername = process.env.Owner_Name || "Pika~Kun" 
-global.ownernumber = process.env.Owner_Number || "916900904828" 
+global.ownername = process.env.Owner_Name || "𝗴𝗼𝗱 𝘇𝗲𝗻𝗶𝘁𝘀𝘂" 
+global.ownernumber = process.env.Owner_Number || "919744108970" 
 global.adress = process.env.Continent || "Asia, India, Assam" 
 global.timezone = process.env.TimeZone || "Asia/Kolkata" 
-global.instagramId = process.env.Insta || "8.08_only_mine" 
-global.email = process.env.Email_Id || "example@example.com" 
+global.instagramId = process.env.Insta || "whoois.zenitsu" 
+global.email = process.env.Email_Id || "hehheeahnndndn@gmail.com " 
   
 //--------------- Tip ----------------\\
 global.Tips = [
@@ -127,8 +127,8 @@ global.Tips = [
 ]
 
 //--------------- Menu images ----------------\\
-global.image_1 = readFileSync('./lib/Assets/image_1.jpg') // Thumbnail for allmenu command
-global.image_2 = readFileSync('./lib/Assets/image_2.jpg') // null image
-global.image_3 = readFileSync("./lib/Assets/image_3.jpg") // Thumbnail for Dashboard
-global.menu_pic = "https://i.ibb.co/PhDcZTM/Thumbnail.png";
+global.image_1 = readFileSync('./lib/IMG-20240329-WA0105.jpg') // Thumbnail for allmenu command
+global.image_2 = readFileSync('./lib/IMG-20240329-WA0105.jpg') // null image
+global.image_3 = readFileSync("./lib/IMG-20240329-WA0105.jpg") // Thumbnail for Dashboard
+global.menu_pic = "https://i.imgur.com/howoEof.jpeg";
 
